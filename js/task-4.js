86a65dacaf33718d5da8a56273360d22f1a08dab
Volodymyr Holodivsky <volodymyrholodivsky@MacBook-Air-1.local>
2024-09-15 12:00:41 +0200

    fix task4

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const form = document.querySelector(`.login-form`);
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const login = event.target.elements.email.valueю.trim();
+  const login = event.target.elements.email.value.trim();
   const password = event.target.elements.password.value.trim();
   if (login === "" || password === "") {
     alert("All form fields must be filled in");
